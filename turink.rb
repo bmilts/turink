008f54b59927433ba2f585872814b78f38d40b61
bmilts <brendan.milton1211@gmail.com>
2018-03-24 15:00:10 +0000

    added a memory storage plus while loops and if statements would ideally like to add variable names also need to go back to the lexer and allow letters as well as numbers

--- turink.rb
+++ turink.rb
@@ -1,11 +1,55 @@
 require "babel_bridge"
 
 # Inherit from Babel bridge parser (alternative to javacc)
-class Parser < BabelBridge::Parser
+class TuringParser < BabelBridge::Parser
     
     # Deal with whitespaces
     ignore_whitespace
     
+    # Store in memory
+    def store
+        # Store starts with an empty array
+        @store||=[]
+    end
+    
+    # Multiple statements
+    # Match as many statements as needed delimited by semicolon
+    rule :statements, many(:statement,";"), match?(";") do
+        # Return last statement evaluated and evaluate in order
+        def evaluate
+          ret = nil
+          statement.each do |s|
+            ret = s.evaluate
+          end
+          ret
+        end
+      end
+    
+    # If statement is 
+    rule :statement, "if", :statement, "then", :statements, :else_clause?, "end" do
+        def evaluate
+            # Matching the 2nd statement "If" 
+            if statement[0].evaluate
+                statement[1].evaluate
+            else
+                else_clause.evaluate if else_clause
+            end 
+        end
+    end
+    
+    rule :else_clause, "else", :statements
+    
+    # While statements
+    # While follwed by statement followed by do followed by final statements then end
+    rule :statement, "while", :statement, "do", :statements, "end" do
+        def evaluate
+            # Use ruby while statement
+            while statement.evaluate
+                statements.evaluate
+            end
+        end
+    end
+
     
     # Parsing rules
     # rule definitions allow blocks using "do" function
@@ -13,14 +57,34 @@ class Parser < BabelBridge::Parser
     # Make add rule recursive by allowing multiple numbers "add" rule 2nd version of add funtion allows int only
     # Added regex symbols using special precedence from left to right 
         # Divisions and times happen first
-    binary_operators_rule :statement, :operand, [[:/, :*], [:+, :-]] do 
-        # Define evaluate on int node
+    binary_operators_rule :statement, :operand, [[:/, :*], [:+, :-], [:<, :<=, :>, :>=, :==]] do
         def evaluate
-            # Babel_bridge takes one int subnode then create method to return value
-            
+          case operator
+          when :<, :<=, :>, :>=, :==
+            (left.evaluate.send operator, right.evaluate) ? 1 : nil
+          else
             left.evaluate.send operator, right.evaluate
+          end
         end
     end
+    
+    # Write to the memory store
+    # With multiple statements first statement gives index
+    rule :operand, "[", :statement, "]", "=", :statement do
+        def evaluate
+            # Each node returns current parser, parser.store accesses store for parser
+            parser.store[statement[0].evaluate] = statement[1].evaluate
+        end
+    end
+    
+    # Read from memory store
+    rule :operand, "[", :statement, "]" do
+        def evaluate
+            # access store and retireve statement value
+            parser.store[statement.evaluate]
+        end
+    end
+    
     
     # Precedence overide
     rule :operand, "(", :statement, ")"
@@ -38,5 +102,4 @@ class Parser < BabelBridge::Parser
 end
 
 # Using babelbridge shell and giving it instance of parser
-BabelBridge::Shell.new(Parser.new).start
-
+BabelBridge::Shell.new(TuringParser.new).start
